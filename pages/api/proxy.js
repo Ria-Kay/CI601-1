d70a6a1da2a6ff9653fad2fd8132d61a1eff6884
Ria Kay <100300568+Ria-Kay@users.noreply.github.com>
2024-11-29 19:27:16 +0000

    Update proxy.js
    api key fixed

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function handler(req, res) {
     const { query, apiKey, endpoint } = req.query; // Example: dynamic query params
-    const API_URL = `https://comicvine.gamespot.com/api/${endpoint}/?api_key=${apiKey}&format=json&sort=name:asc&resources=issue&query=${query}`;
+    const API_URL = `https://comicvine.gamespot.com/api/$search/?api_key=${de2d10e13b5fca21fdf1b9c321676937e104e57b }&format=json&sort=name:asc&resources=issue&query=${query}`;
 
     try {
         const response = await axios.get(API_URL);

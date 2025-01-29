af2658b09c058cc8235b0cf7bbd89a8d3f622c47
Ria-Kay <100300568+Ria-Kay@users.noreply.github.com>
2025-01-29 22:47:35 +0000

    fixing lack of react functions

--- pages/usercomics.js
+++ pages/usercomics.js
@@ -1,4 +1,20 @@
 import { useState } from 'react';
-//import Header from '../components/Header';
-//import global.css from
+// import Header from '../components/Header';
+// import global.css from (fix this import)
 
+function UserComicsPage() {
+  const [comics, setComics] = useState([]);
+
+  return (
+    <div>
+      <h1>User Comics</h1>
+      {comics.length > 0 ? (
+        comics.map((comic, index) => <p key={index}>{comic}</p>)
+      ) : (
+        <p>No comics found.</p>
+      )}
+    </div>
+  );
+}
+
+export default UserComicsPage;

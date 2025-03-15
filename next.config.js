7471cf2df4bc4a72fe249043dcfa23a3b8205673
Ria-Kay <100300568+Ria-Kay@users.noreply.github.com>
2025-03-15 16:54:14 +0000

    added config in root

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: false, // Ensures Vercel does not append .html
+    output: "standalone" // Forces Vercel to use SSR instead of static HTML export
+  };
+  
+  module.exports = nextConfig;
+
+  
+//check the stack overflow

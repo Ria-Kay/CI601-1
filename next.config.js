84a7cd44f787ccb4d376bc248018fc9e4e2c9208
Ria Kumar <riakumar28@outlook.com>
2025-03-24 18:18:00 +0000

    small fixes

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     trailingSlash: false, // Ensures Vercel does not append .html
     output: "standalone", // Forces Vercel to use SSR instead of static HTML export
-    experimental: {
-        appDir: false, // Ensures proper routing
-      }
+    
 
 };
   

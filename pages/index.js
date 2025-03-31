5afc9d879bd2f0160126ec240511eee29e26885b
Ria-Kay <100300568+Ria-Kay@users.noreply.github.com>
2025-04-01 00:13:54 +0100

    png for img

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main>
         <div className="home-intro">
           //this will allow for lazy loading so its not as slow when the api will run for issues too
-        <Image src="/comichunt.svg" alt="ComicHunt logo" width={200} height={80} />
+        <Image src="/comichunt.png" alt="ComicHunt logo" width={200} height={80} />
         <h1>Welcome to ComicHunt </h1>
           <p>A Visual Archive For All Your Comcis</p>
         </div>

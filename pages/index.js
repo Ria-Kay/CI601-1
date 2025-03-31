953e026d2eea42968410dce8257db624a2cdecba
Ria-Kay <100300568+Ria-Kay@users.noreply.github.com>
2025-04-01 00:15:37 +0100

    img folder fix

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
 
       <main>
         <div className="home-intro">
-          //this will allow for lazy loading so its not as slow when the api will run for issues too
-        <Image src="/comichunt.png" alt="ComicHunt logo" width={200} height={80} />
+          {/*this will allow for lazy loading so its not as slow when the api will run for issues too*/}
+        <Image src="/images/comichunt.png" alt="ComicHunt logo" width={200} height={80} />
         <h1>Welcome to ComicHunt </h1>
           <p>A Visual Archive For All Your Comcis</p>
         </div>

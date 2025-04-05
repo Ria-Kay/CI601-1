29fe18c168d264fee4751a2d35199fed50608112
Ria Kumar <riakumar28@outlook.com>
2025-04-05 16:43:42 +0100

    index c ss

--- pages/index.js
+++ pages/index.js
@@ -12,36 +12,42 @@ export default function Home() {
       <main>
         {/* Welcome Section */}
         <section className="home-section intro-section">
-  <div className="sectionBox introBox">
-    <div className="intro-content">
-      <div className="image-wrapper logo-large">
+          <div className="sectionBox introBox">
+            <div className="intro-content">
+              <div className="image-wrapper logo-large">
+                <Image
+                  src="/images/updatedlogo.svg"
+                  alt="ComicHunt logo"
+                  width={320}
+                  height={320}
+                  className="biglogo"
+                />
+              </div>
 
-<Image
-  src="/images/updatedlogo.svg"
-  alt="ComicHunt logo"
-  width={320}
-  height={320}
-  className="biglogo"
-/>      </div>
+              <div className="intro-text">
+                <h1 className="intro-heading">Find, Log, Store</h1>
+                <h2 className="intro-subheading">Welcome to ComicHunt</h2>
+                <p className="intro-tagline">A Visual Archive For All Your Comics</p>
+              </div>
+            </div>
+          </div>
+        </section>
 
-      <div className="intro-text">
-        <h1 className="intro-heading">Find, Log, Store</h1>
-        <h2 className="intro-subheading">Welcome to ComicHunt</h2>
-        <p className="intro-tagline">A Visual Archive For All Your Comics</p>
-      </div>
-    </div>
-  </div>
-</section>
-
-
-
-        {/* Store Section (Agatha Harkness) */}
+        {/* Store Section */}
         <section className="home-section">
           <div className="sectionBox">
             <div className="image-wrapper">
               <Image
-                src="/images/Agatha_Harkness_29.png"
+                src="/images/AH2.svg"
                 alt="Comic Character - Agatha Harkness"
+                fill
+              />
+            </div>
+
+            <div className="image-wrapper">
+              <Image
+                src="/images/indexdp.png"
+                alt="Image of the Your Comics page"
                 fill
               />
             </div>
@@ -49,23 +55,87 @@ export default function Home() {
             <div className="section-text">
               <h1>Store your comics</h1>
               <h3>
-              At ComicHunt, we know that comic collecting is more than just a hobby—it's a passion. Our intuitive database allows you to easily log and manage your entire collection, while also contributing to a growing community of fellow collectors.              </h3>
+                At ComicHunt, we know that comic collecting is more than just a hobby—it's a passion. Our intuitive database allows you to easily log and manage your entire collection, while also contributing to a growing community of fellow collectors.
+              </h3>
               <h3>
-              Whether you're tracking individual issues, organizing by series, or tagging storage locations like short and long boxes, ComicHunt is designed to reflect the way you collect in the real world. You can even flag missing issues or curate your own custom groups to keep everything in order.              </h3>
+                Whether you're tracking individual issues, organizing by series, or tagging storage locations like short and long boxes, ComicHunt is designed to reflect the way you collect in the real world. You can even flag missing issues or curate your own custom groups to keep everything in order.
+              </h3>
             </div>
           </div>
         </section>
 
-        {/* Latest Issues Section */}
+        {/* Dynamic Visual Analytics Section */}
         <section className="home-section">
-        <div className="sectionBox">
-        <div className= "new-issues">
-            <LatestComics />
-        </div>
-        </div>
+          <div className="sectionBox">
+            <div className="image-wrapper">
+              <Image
+                src="/images/indexdp.png"
+                alt="Image of analytics view"
+                fill
+              />
+            </div>
+
+            <div className="section-text">
+              <h1>Dynamic Visual Analytics</h1>
+              <h3>
+                At ComicHunt, we know that comic collecting is more than just a hobby—it's a passion. Our intuitive database allows you to easily log and manage your entire collection, while also contributing to a growing community of fellow collectors.
+              </h3>
+              <h3>
+                Whether you're tracking individual issues, organizing by series, or tagging storage locations like short and long boxes, ComicHunt is designed to reflect the way you collect in the real world. You can even flag missing issues or curate your own custom groups to keep everything in order.
+              </h3>
+            </div>
+
+            <div className="image-wrapper">
+              <Image
+                src="/images/AH2.svg"
+                alt="Comic Character - Agatha Harkness"
+                fill
+              />
+            </div>
+          </div>
         </section>
 
-        {/* New Users Section */}
+        {/* Comic Vine API Section */}
+        <section className="home-section">
+          <div className="sectionBox">
+            <div className="image-wrapper">
+              <Image
+                src="/images/CVlogo.png"
+                alt="Comic Vine logo"
+                fill
+              />
+            </div>
+
+            <div className="section-text">
+              <h1>Built using the Comic Vine API</h1>
+              <h3>
+                At ComicHunt, we know that comic collecting is more than just a hobby—it's a passion. Our intuitive database allows you to easily log and manage your entire collection, while also contributing to a growing community of fellow collectors.
+              </h3>
+              <h3>
+                Whether you're tracking individual issues, organizing by series, or tagging storage locations like short and long boxes, ComicHunt is designed to reflect the way you collect in the real world. You can even flag missing issues or curate your own custom groups to keep everything in order.
+              </h3>
+            </div>
+
+            <div className="image-wrapper">
+              <Image
+                src="/images/CCC.svg"
+                alt="Comic graphic"
+                fill
+              />
+            </div>
+          </div>
+        </section>
+
+        {/* Latest Issues */}
+        <section className="home-section">
+          <div className="sectionBox">
+            <div className="new-issues">
+              <LatestComics />
+            </div>
+          </div>
+        </section>
+
+        {/* Newest Users Section */}
         <section className="home-section">
           <div className="sectionBox">
             <div className="image-wrapper">
@@ -81,6 +151,14 @@ export default function Home() {
               <h3>
                 Welcome our latest comic hunters to the community! Sign up and see who else shares your favorite series, or discover what your fellow fans are reading.
               </h3>
+            </div>
+
+            <div className="image-wrapper">
+              <Image
+                src="/images/JN!.svg"
+                alt="Comic Graphic - Join Now"
+                fill
+              />
             </div>
           </div>
         </section>

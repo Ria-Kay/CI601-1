85e023e48df3e283b67965e0471ab57a2fa711c6
Ria-Kay <100300568+Ria-Kay@users.noreply.github.com>
2025-05-15 23:17:42 +0100

    footer citations for relavent sources

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,24 @@ export default function Footer() {
     <footer className="footer">
       <p>© {new Date().getFullYear()} ComicHunt. All rights reserved.</p>
       <p>
-        Built with ComicVine API
+        Built with ComicVine API, to source all comic metadata. https://comicvine.gamespot.com/api/
       </p>
       <p>
         Built By Ria Kumar
+      </p>
+      <p>
+        Icons from Icons8, Agatha Harkness character images sourced from Marvel Studios, 2024.
+      </p>
+      <p>
+        Nightwing character images sourced from Detective Comics, 2025.
+      </p>
+      <P>
+        Comic hunt was built as a part of this students final year project, all materials used are used for the purpose of eduction only.
+      </P>
+
+      <p>
+        https://hype4.academy/tools/glassmorphism-generator
+        https://github.com/vasturiano/3d-force-graph?tab=MIT-1-ov-file
       </p>
 
     </footer>
